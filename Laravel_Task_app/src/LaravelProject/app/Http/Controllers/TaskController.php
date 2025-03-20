32fa747bb49391195275e52cfca615ae9e3d91c9
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2025-03-21 02:33:49 +0900

    フォルダパネルの表示まで

--- Laravel_Task_app/src/LaravelProject/app/Http/Controllers/TaskController.php
+++ Laravel_Task_app/src/LaravelProject/app/Http/Controllers/TaskController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Folder;
 
 class TaskController extends Controller
 {
     // index メソッドを追加する
     public function index()
     {
-        return "Hello world";
+        
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
