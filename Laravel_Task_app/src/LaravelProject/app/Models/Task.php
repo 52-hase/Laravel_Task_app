b580594958956d6627a2b69c8b56821c54154557
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2025-03-24 02:40:36 +0900

    状態の色分けまで

--- Laravel_Task_app/src/LaravelProject/app/Models/Task.php
+++ Laravel_Task_app/src/LaravelProject/app/Models/Task.php
@@ -8,4 +8,42 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    // タスクのステータス（状態）を定義
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
+    ];
+    
+    // ステータスのラベルを取得するメソッド
+    public function getStatusLabelAttribute()
+    {
+        // タスクのステータス値を取得
+        $status = $this->attributes['status'];
+
+        // 定義されたステータスに存在しない場合は空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        // 該当するステータスのラベルを返す
+        return self::STATUS[$status]['label'];
+    }
+
+    /**
+     * 状態を表すHTMLクラスのアクセサメソッド
+     * 
+     * @return string
+     */
+    public function getStatusClassAttribute()
+    {
+        $status = $this->attributes['status'];
+
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
 }
