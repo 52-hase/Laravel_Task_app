a3aa6205120217a8e0bf0ddd4321880a1fb68e52
tetsuya hasegawa <ttea.oo.cl@gmail.com>
2025-03-24 02:45:26 +0900

    日付の表示 / に変更

--- Laravel_Task_app/src/LaravelProject/app/Models/Task.php
+++ Laravel_Task_app/src/LaravelProject/app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;  // Carbon ライブラリを名前空間でインポートする
 
 class Task extends Model
 {
@@ -45,5 +46,18 @@ class Task extends Model
         }
 
         return self::STATUS[$status]['class'];
+    }
+
+   
+
+    /**
+     * 整形した期限日のアクセサメソッド
+     * 
+     * @return string
+     */
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])
+            ->format('Y/m/d');
     }
 }
